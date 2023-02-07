bb6eb70b3cf7a5066fd279db3d063bd3bb200736
fardin-faisal <fardin00faisal05@gmail.com>
2023-02-07 23:50:20 +0600

    Banking System

--- main.py
+++ main.py
@@ -1,4 +1,34 @@
-print("Hello world")
-print("Added from faisal branch")
+class Bank_system:
+    def __init__(Faisal, balance):
+        Faisal.balance = balance
+        Faisal.min_withdraw = 1000
+        Faisal.max_withdraw = 100000
 
-# open termical shortcut (ctrl + ~)
+    def get_balance(Faisal):
+        return Faisal.balance
+
+    def withdraw (Faisal , amount):
+        if amount < Faisal.min_withdraw:
+            return f"no money for you. Minimum need to take {Faisal.min_withdraw} taka"
+        elif amount > Faisal.max_withdraw:
+            return f"crosses max limit : {Faisal.max_withdraw}"
+        elif amount > Faisal.balance:
+            return "No money sorry"
+        else:
+            Faisal.balance = Faisal.balance - amount
+            return f"Here is yuor money : {amount}"
+
+    def deposit(Faisal, amount):
+        Faisal.balance = Faisal.balance + amount
+
+bal = int(input('Enter Balance: '))
+my_bank = Bank_system(bal)
+
+wit = int(input('Enter the withdraw ammount: '))
+f"Withdrawed ammount : {my_bank.withdraw(wit)} tk"
+print(my_bank.get_balance())
+
+
+dep = int(input('Enter Deposit_Balance: '))
+my_bank.deposit(dep)
+print(my_bank.get_balance())
